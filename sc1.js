b5842ef73b90ce72a3565a9926234c3353020c59
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2019-07-11 14:39:24 +0400

    Update sc1.js

--- sc1.js
+++ sc1.js
@@ -4,7 +4,7 @@ var b=prompt("Vvedy chislo 2", " ");
 var c=prompt("Vvedy deystvie", " ");
 var znach;
 if (c=="+"){
-//znach= (+a) + (+b);
+
 alert(a +b);
 }
  else if (c=="-"){
@@ -12,15 +12,15 @@ alert(a -b);
 }
  else if (c=="*"){
  	alert(a *b);
-//znach= (+a)*(+b);
+
 }
  else if (c=="/"){
  	alert(a /b);
-//znach= (+a)/(+b);
+
 }
 else if (c=="%"){
 	alert(a%b);
-//znach=(+a)%(+b);
+
 }
 
 
