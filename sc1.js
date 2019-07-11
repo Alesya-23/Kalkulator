525d868f606bf220cc0f358ed2b9833d81826ca2
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2019-07-11 14:29:19 +0400

    Add files via upload

---
+++ sc1.js
@@ -0,0 +1,27 @@
+//prompt("Vvedy chisla i operaciu");
+var a=prompt("Vvedy chislo 1", " ");
+var b=prompt("Vvedy chislo 2", " ");
+var c=prompt("Vvedy deystvie", " ");
+var znach;
+if (c=="+"){
+//znach= (+a) + (+b);
+alert(a +b);
+}
+ else if (c=="-"){
+alert(a -b);
+}
+ else if (c=="*"){
+ 	alert(a *b);
+//znach= (+a)*(+b);
+}
+ else if (c=="/"){
+ 	alert(a /b);
+//znach= (+a)/(+b);
+}
+else if (c=="%"){
+	alert(a%b);
+//znach=(+a)%(+b);
+}
+
+
+
